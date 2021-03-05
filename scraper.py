b637c020ee6df98e717d3801afc7f60c03e551eb
GeoWill <roper.william@gmail.com>
2021-03-05 08:16:54 +0000

    Update URL

--- scraper.py
+++ scraper.py
@@ -2,11 +2,10 @@ from dc_base_scrapers.geojson_scraper import GeoJsonScraper
 from dc_base_scrapers.hashonly_scraper import HashOnlyScraper
 
 
-search_url = "https://opendata.arcgis.com/api/v2/datasets?filter%5Bcatalogs%5D=data-moray.opendata.arcgis.com&include=organizations%2Cgroups&page%5Bnumber%5D=1&page%5Bsize%5D=10&q=polling&sort=-updatedAt"
+search_url = "https://opendata.arcgis.com/api/v3/datasets?q=polling&filter%5Bowner%5D=gis_moray"
 stations_url = "https://opendata.arcgis.com/datasets/65e731a73f384fadad25bef39fdcc342_0.geojson"
 districts_url = "https://opendata.arcgis.com/datasets/bd878bfec39f4a659c2d016f1b3bc920_1.geojson"
-council_id = 'S12000020'
-
+council_id = 'MRY'
 
 search_scraper = HashOnlyScraper(search_url, council_id, 'datasets', 'json')
 search_scraper.scrape()
